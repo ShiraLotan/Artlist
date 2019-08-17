76b0088766e57069e42ef7370397eab094bfa9fd
Your Name <you@example.com>
2019-08-17 21:22:28 +0300

    logo

--- src/main/top.js
+++ src/main/top.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import topImg from '../img/top1.png';
 import arrow from '../img/arrowDown.png';
-
+import artlist from '../img/artlist1.png';
 import './top.css';
 
 function Top() {
@@ -10,6 +10,7 @@ function Top() {
     <div className="top">
       <Container  fixed>
         <div className='topCont'>
+          <img  className='artlist' src={artlist} alt='#'/>
             <h1 className='header'>We want your inspiring music</h1>
             <p className='secondRow'>Get your music into films, videos & TV all around the world. Join the Artlist family.</p>
             <button className='downloadButton'>
